058c2df0b9da346f408e67397d0ae13ebeb351f4
Cody Averett <codyaverett@gmail.com>
2023-12-23 00:46:47 -0600

    style: fmt

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,50 @@
-import * as React from 'react';
-import { render, Box, Text } from 'ink';
+import * as React from "react";
+import { render, Box, Text } from "ink";
+
+/**
+ * add_signal_listener.ts
+ */
+console.log("Press Ctrl-C to trigger a SIGINT signal");
+
+Deno.addSignalListener("SIGINT", () => {
+  console.log("interrupted!");
+  Deno.exit();
+});
+
+// Add a timeout to prevent process exiting immediately.
+setTimeout(() => {}, 5000);
+
+const Meta = () => {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((previousCount) => previousCount + 1);
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return <Text color="green">{count} tests passed</Text>;
+};
 
 const App = () => {
   const data = {
-    name: 'John Doe',
+    name: "John Doe",
     age: 30,
-    occupation: 'Software Developer'
+    occupation: "Software Developer",
   };
 
   return (
-    <Box>
-      <Text>
-        {JSON.stringify(data, null, 2)}
-      </Text>
+    <Box flexDirection="column">
+      <Meta />
+      <Text>Hello, world!</Text>
+      <Text>This is a demo of Ink.</Text>
+      <Box>
+        <Text>{JSON.stringify(data, null, 2)}</Text>
+      </Box>
     </Box>
   );
 };
